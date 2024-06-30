f4811d72331519d8e96960aba1cc4f19f023bd54
Rehaan Ahmad <rehaansufyaan1@gmail.com>
2024-06-30 11:47:11 +0530

    question

--- src/leetcode_practice/Find.java
+++ src/leetcode_practice/Find.java
@@ -1,2 +1,19 @@
-package leetcode_practice;public class Find {
+package leetcode_practice;
+
+public class Find {
+  static   int chae(String first,String sec) {
+      if (first.length() < sec.length()) {
+          return -1;
+      }
+      for (int a = 0; a <= first.length() - sec.length(); a++) {
+          if (first.substring(a, a + sec.length()).equals(sec)) {
+              return a;
+          }
+      }
+      return -1;
+  }
+    public static void main(String[] args) {
+     int ans=chae("rehaan","reh");
+        System.out.println(ans);
+    }
 }
